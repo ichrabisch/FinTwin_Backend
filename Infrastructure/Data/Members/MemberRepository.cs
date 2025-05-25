2999ece003d23a05a9ac67ed23bba06fb85d5692
ichrabisch <rabiaa19@outlook.com.tr>
2025-05-25 22:45:30 +0300

    Add MemberRepository implementation for managing member data, including methods for creating and retrieving members by ID and email.

---
+++ Infrastructure/Data/Members/MemberRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Members.Model;
+using Domain.Members.Repository;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel.Abstractions;
+
+namespace Infrastructure.Data.Members;
+
+public class MemberRepository(IDbOperations context, IUnitOfWork unitOfWork) : Repository<Member>(context, unitOfWork), IMemberRepository
+{
+    public async Task Create(Member member, CancellationToken ct)
+    {
+        await AddAsync(member, ct);
+    }
+
+    public async Task<Member> Retrive(Guid id, CancellationToken ct)
+    {
+        return await GetByIdAsync(id, ct)
+            ?? throw new KeyNotFoundException($"Member with id {id} not found.");
+    }
+
+    public async Task<Member?> Retrive(string email, CancellationToken ct)
+    {
+        return await _context.Set<Member>()
+            .FirstOrDefaultAsync(m => m.Email == email, ct);
+    }
+}
