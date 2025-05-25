bd80a1b203fb437f7c22f0ce421e9eac6ec30566
ichrabisch <rabiaa19@outlook.com.tr>
2025-05-25 22:57:32 +0300

    Add IMemberRepository interface for member data management. Define methods for creating and retrieving members by ID and email, enhancing the repository layer's capabilities for member operations.

---
+++ src/domain/Members/Repository/IMemberRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Members.Model;
+
+namespace Domain.Members.Repository;
+
+public interface IMemberRepository
+{
+    public Task Create(Member member, CancellationToken ct);
+    public Task<Member> Retrive(Guid id, CancellationToken ct);
+    public Task<Member?> Retrive(string email, CancellationToken ct);
+}
