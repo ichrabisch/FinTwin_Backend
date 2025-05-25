aa3605a84de61e2233c68bcc790dcf4b9b488901
ichrabisch <rabiaa19@outlook.com.tr>
2025-05-25 22:49:02 +0300

    Add RetriveGeneralCategories command and handler for retrieving categorized transaction totals. Introduce DTO for category data, enhancing account management capabilities.

---
+++ src/Application/Accounts/Commands/RetriveGeneralCategories/RetriveGeneralCategoriesCommand.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+using SharedKernel.Abstractions;
+
+namespace Application.Accounts.Commands.RetriveGeneralCategories;
+
+public sealed record RetriveGeneralCategoriesCommand(
+        Guid AccountId,
+        DateTime? StartDate,
+        DateTime? EndDate
+    ) : ICommand<Result<List<RetriveGeneralCategoriesDto>>>;
+
+public sealed record RetriveGeneralCategoriesDto(string Category, decimal TotalAmount);
---
+++ src/Application/Accounts/UseCases/RetriveGeneralCategoriesCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Accounts.Commands.RetriveGeneralCategories;
+using AutoMapper;
+using Domain.Accounts.Repository;
+using FluentResults;
+using SharedKernel.Abstractions;
+
+namespace Application.Accounts.UseCases;
+
+public class RetriveGeneralCategoriesCommandHandler : ICommandHandler<RetriveGeneralCategoriesCommand, Result<List<RetriveGeneralCategoriesDto>>>
+{
+    private readonly IAccountRepository _accountRepository;
+    
+    public RetriveGeneralCategoriesCommandHandler(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
+    public async Task<Result<List<RetriveGeneralCategoriesDto>>> Handle(RetriveGeneralCategoriesCommand command, CancellationToken cancellationToken)
+    {
+        var transactions = await _accountRepository.GetTransactionsByAccountIdAsync(
+                                                    command.AccountId,
+                                                    command.StartDate,
+                                                    command.EndDate,
+                                                    null,
+                                                    cancellationToken);
+
+        var receipts = transactions.Select(t => t.Receipt).ToList();
+        Dictionary<string, decimal> categories = new Dictionary<string, decimal>();
+        foreach(var reciept in receipts)
+        {
+            if(reciept == null)
+            {
+                continue;
+            }
+            foreach(var item in reciept!.Items)
+            {
+                categories[item.GeneralCategory] = categories.ContainsKey(item.GeneralCategory) ? categories[item.GeneralCategory] + item.TotalPrice : item.TotalPrice;
+            }
+        }
+        var categoriesDtos = categories.Select(c => new RetriveGeneralCategoriesDto(c.Key, c.Value)).ToList();
+        return Result.Ok(categoriesDtos);
+    }
+}
