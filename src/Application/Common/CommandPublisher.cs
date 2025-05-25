d94e65104b535ebeb174ccfe5a82f0c25b1c021c
ichrabisch <rabiaa19@outlook.com.tr>
2025-05-25 22:49:54 +0300

    Add CommandPublisher class for handling command execution via dependency injection. Implements ICommandPublisher interface to publish commands and retrieve their responses, ensuring proper handler registration.

---
+++ src/Application/Common/CommandPublisher.cs
@@ -0,0 +1,21 @@
+﻿using SharedKernel.Abstractions;
+
+namespace Application.Common;
+
+public sealed class CommandPublisher(IServiceProvider serviceProvider) : ICommandPublisher
+{
+    private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+    public async Task<TResponse> Publish<TResponse, TCommand>(TCommand command, CancellationToken cancellationToken)
+         where TCommand : class, ICommand<TResponse>
+    {
+        var useCaseHandler =
+           (ICommandHandler<TCommand, TResponse>?)
+           _serviceProvider.GetService(typeof(ICommandHandler<TCommand, TResponse>));
+
+
+        return useCaseHandler == null
+            ? throw new InvalidOperationException($"No handler registered for use case of type {typeof(TCommand)}")
+            : await useCaseHandler.Handle(command, cancellationToken);
+    }
+}
