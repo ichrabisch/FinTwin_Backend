d98cfe3e2a7902ee74b69d3a4d30ac1e953492c0
ichrabisch <rabiaa19@outlook.com.tr>
2025-05-25 22:56:56 +0300

    Add Address value object to represent location details. Include properties for street, city, country, and optional latitude and longitude, enhancing domain modeling for address management.

---
+++ src/domain/Common/ValueObjects/Address.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Common.ValueObjects;
+
+public sealed record Address
+{
+    public Address(string street, string city, string country)
+    {
+        Street = street;
+        City = city;
+        Country = country;
+    }
+
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string Country { get; private set; }
+    public float? Latitude { get; internal set; }
+    public float? Longitude { get; internal set; }
+
+}
