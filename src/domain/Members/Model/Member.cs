c296b98a91f7123e8edd21650d70b6ce0d5b1221
ichrabisch <rabiaa19@outlook.com.tr>
2025-05-25 22:57:17 +0300

    Add Member and MemberProfile domain models for member management. Implement core properties and methods for member creation, profile management, and account handling, enhancing the domain layer's capabilities for user management.

---
+++ src/domain/Members/Model/Member.cs
@@ -0,0 +1,72 @@
+﻿using Domain.Accounts.Model;
+using Domain.Common.ValueObjects;
+using Domain.Members.ValueObjects;
+using SharedKernel.Abstractions;
+using SharedKernel.Common;
+using SharedKernel.Primitives;
+
+namespace Domain.Members.Model;
+
+public class Member : AggregateRoot, IDeletableEntity, IAuditableEntity
+{
+    private Member(Guid id, string email, string password, AccountType accountType) : base(id)
+    {
+        Email = email;
+        Password = password;
+        AccountType = accountType;
+    } 
+    public List<Account> Accounts { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public AccountType AccountType { get; private set; }
+    public MemberProfile MemberProfile { get; private set; }
+
+    public bool IsDeleted { get; private set;}
+
+    public DateTime? DeletedAt { get; private set;}
+
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; private set; }
+
+    public static Member Create(Guid id, string email, string password, AccountType accountType)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(email);
+        ArgumentException.ThrowIfNullOrEmpty(password);
+
+        var emailResult = GuardClause.Ensure.IsValidEmail(email);
+
+        if (!emailResult.IsSuccess) throw new ArgumentException(emailResult.Reasons[0].ToString());
+
+        return new Member(id, email, password, accountType);
+    }
+
+    public void AddProfile(
+        string name,
+        string surname,
+        EducationInformation educationLevel,
+        EmploymentInformation employmentStatus,
+        Address address,
+        ResidentialInformation residentialInformation,
+        FamilySize familySize)
+    {
+        if (MemberProfile != null) throw new InvalidOperationException("Profile already exists");
+
+        MemberProfile = MemberProfile.Create(
+            Guid.NewGuid(),
+            name,
+            surname,
+            educationLevel,
+            employmentStatus,
+            address,
+            residentialInformation,
+            familySize,
+            this);
+    }
+
+    public void AddAccount(Account account)
+    {
+        Accounts.Add(account);
+    }
+
+}
---
+++ src/domain/Members/Model/MemberProfile.cs
@@ -0,0 +1,75 @@
+﻿using Domain.Common.ValueObjects;
+using Domain.Members.ValueObjects;
+using SharedKernel.Abstractions;
+using SharedKernel.Primitives;
+
+namespace Domain.Members.Model;
+
+public class MemberProfile : Entity, IAuditableEntity, IDeletableEntity
+{
+    protected MemberProfile() : base(Guid.NewGuid()) { } // This constructor is for EF Core
+
+    public MemberProfile(
+            Guid id,
+            string name,
+            string surname,
+            EducationInformation educationLevel,
+            EmploymentInformation employmentStatus,
+            Address address,
+            ResidentialInformation residentialInformation,
+            FamilySize familySize,
+            Member member
+        ) : base(id)
+    {
+        Name = name;
+        Surname = surname;
+        EducationLevel = educationLevel;
+        EmploymentStatus = employmentStatus;
+        Address = address;
+        ResidentialInformation = residentialInformation;
+        FamilySize = familySize;
+        Member = member;
+        MemberId = member.Id;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public static MemberProfile Create(
+               Guid id,
+               string name,
+               string surname,
+               EducationInformation educationLevel,
+               EmploymentInformation employmentStatus,
+               Address address,
+               ResidentialInformation residentialInformation,
+               FamilySize familySize,
+               Member member
+           )
+    {
+        return new MemberProfile(id, name, surname, educationLevel, employmentStatus, address, residentialInformation, familySize, member);
+    }
+
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public EducationInformation EducationLevel { get; private set; }
+    public EmploymentInformation EmploymentStatus { get; private set; }
+    public Address Address { get; private set; }
+    public ResidentialInformation ResidentialInformation { get; private set; }
+    public FamilySize FamilySize { get; private set; }
+    public int? EconomicLevelId { get; private set; }
+    public Member Member { get; private set; }
+    public Guid MemberId { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+    public bool IsDeleted { get; private set; }
+    public DateTime? DeletedAt { get; private set; }
+
+    public void SetLocation(float latitude, float longitude)
+    {
+        Address.Latitude = latitude;
+        Address.Longitude = longitude;
+    }
+    public void SetEconomicLevel(int economicLevel)
+    {
+        EconomicLevelId = economicLevel;
+    }
+}
