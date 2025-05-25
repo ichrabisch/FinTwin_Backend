0eb82b6140df853e7fd2739bb2961045b8258bb8
ichrabisch <rabiaa19@outlook.com.tr>
2025-05-25 22:58:17 +0300

    Add CommandBuilder and GuardClause classes to SharedKernel. Implement CommandBuilder for dynamic command creation and GuardClause for email validation, enhancing command handling and input validation capabilities.

---
+++ src/SharedKernel/Common/CommandBuilder.cs
@@ -0,0 +1,35 @@
+﻿using SharedKernel.Abstractions;
+using System.Linq.Expressions;
+
+namespace SharedKernel.Common;
+public class CommandBuilder<TCommand> where TCommand : class, new()
+{
+    private readonly Dictionary<string, object> _propertyValues = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+    public CommandBuilder<TCommand> With<TProperty>(Expression<Func<TCommand, TProperty>> property, TProperty value)
+    {
+        if (!(property.Body is MemberExpression memberExpression))
+        {
+            throw new ArgumentException("Expression must be a member access", nameof(property));
+        }
+        string propertyName = memberExpression.Member.Name;
+        _propertyValues[propertyName] = value;
+        return this;
+    }
+
+    public TCommand Build()
+    {
+        var command = new TCommand();
+        var properties = typeof(TCommand).GetProperties();
+
+        foreach (var prop in properties)
+        {
+            if (_propertyValues.TryGetValue(prop.Name, out var value))
+            {
+                prop.SetValue(command, value);
+            }
+        }
+
+        return command;
+    }
+}
---
+++ src/SharedKernel/Common/GuardClause.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using FluentResults;
+
+namespace SharedKernel.Common;
+public static class GuardClause
+{
+    public static class Ensure 
+    {
+        public static Result<string> IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return Result.Fail<string>("Email cannot be null or empty.");
+            }
+            try
+            {
+                var regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", RegexOptions.IgnoreCase);
+                if (regex.IsMatch(email))
+                {
+                    return Result.Ok(email);
+                }
+                else
+                {
+                    return Result.Fail<string>("Invalid email format.");
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return Result.Fail<string>("Email validation timed out.");
+            }
+        }
+    }
+}
